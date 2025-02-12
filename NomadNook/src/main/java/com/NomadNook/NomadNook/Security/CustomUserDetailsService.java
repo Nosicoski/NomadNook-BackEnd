2318c0bb2b8a15f2ffa1f88b7382522ce3b4bf2c
jioh19 <jioh1982@gmail.com>
2025-02-12 20:26:17 -0300

    JWT basico done

---
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.NomadNook.NomadNook.Security;
+
+import com.NomadNook.NomadNook.Model.Usuario;
+import com.NomadNook.NomadNook.Repository.IUsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new User(
+                usuario.getEmail(),
+                usuario.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_" + usuario.getRol().name()))
+        );
+    }
+}
