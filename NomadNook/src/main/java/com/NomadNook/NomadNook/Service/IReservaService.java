d9e99198d72558772b486c49632a6ea788ff17ec
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-12 18:45:07 -0300

    Interfaces del service

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IReservaService.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IReservaService.java
@@ -1,4 +1,13 @@
 package com.NomadNook.NomadNook.Service;
 
+import com.NomadNook.NomadNook.Model.Reserva;
+
+import java.util.List;
+
 public interface IReservaService {
+    Reserva createReserva(Reserva reserva);
+    Reserva getReservaById(Long id);
+    List<Reserva> listReservasByCliente(Long clienteId);
+    Reserva updateReserva(Long id, Reserva reserva);
+    void deleteReserva(Long id);
 }
