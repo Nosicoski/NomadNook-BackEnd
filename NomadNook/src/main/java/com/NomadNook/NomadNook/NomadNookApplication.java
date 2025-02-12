c720b07065bfd1e90b5a1ceffa3cf6595b1c2775
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-11 21:15:05 -0300

    first commit

---
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/NomadNookApplication.java
@@ -0,0 +1,13 @@
+package com.NomadNook.NomadNook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadNookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadNookApplication.class, args);
+	}
+
+}
---
+++ NomadNook/src/test/java/com/NomadNook/NomadNook/NomadNookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NomadNook.NomadNook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadNookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
