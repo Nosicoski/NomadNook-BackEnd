d307849857c27cdb5362b25c1710c1932ccb3802
jioh19 <jioh1982@gmail.com>
2025-02-17 15:35:53 -0300

    Sin builder v1.1

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Controller/UsuarioController.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Controller/UsuarioController.java
@@ -20,7 +20,7 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<Usuario>> todosUser() {
         return ResponseEntity.ok(usuarioService.listAllUsers());
     }
