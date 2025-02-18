1c8ded11a0f5e6651658cbea4120528b2b576d7b
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-18 00:06:43 -0300

    .

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Controller/ReservaController.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Controller/ReservaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Reserva")
+@RequestMapping("/api/Reservas")
 public class ReservaController {private final IReservaService reservaService;
 
     public ReservaController(IReservaService reservaService) {
