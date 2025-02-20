8118750aeba9284bd9dcaa7e49576683fa7ebf8f
Ignacio Jorge Arévalo <ignacioarevalo@gmail.com>
2025-02-20 20:40:49 -0300

    ValidationExceptionHandler extends Exception corregido

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Exception/ValidationExceptionHandler.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Exception/ValidationExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class ValidationExceptionHandler {
+public class ValidationExceptionHandler extends Exception {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
