df0435869b70876a844d6b902ffdcbb4e795d9e4
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-21 19:44:38 -0300

    Alojamiento Request y Alojamiento Response

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Security/DTO/RESPONSE/AlojamientoResponse.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Security/DTO/RESPONSE/AlojamientoResponse.java
@@ -1,4 +1,31 @@
 package com.NomadNook.NomadNook.Security.DTO.RESPONSE;
 
+import com.NomadNook.NomadNook.Model.Alojamiento;
+import com.NomadNook.NomadNook.Model.Imagen;
+import com.NomadNook.NomadNook.Model.Usuario;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlojamientoResponse {
+    private String titulo;
+    private String descripcion;
+    private Alojamiento.TipoAlojamiento tipo;
+    private Integer capacidad;
+    private BigDecimal precioPorNoche;
+    private String ubicacion;
+    private String direccion;
+    private Boolean disponible;
+    private Usuario propietario;
+    private List<Imagen> imagenes;
+    public enum TipoAlojamiento {
+        PLAYA,MONTANA,NEVADA,SELVA,BOSQUE,CAMPO
+    }
 }
