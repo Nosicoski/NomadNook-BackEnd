995259232783f0fac42bd73ed8f5dfe3f5aa6381
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-21 19:53:24 -0300

    Anotaciones y disponibilidad Request and Response

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Security/DTO/RESPONSE/ReservaResponse.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Security/DTO/RESPONSE/ReservaResponse.java
@@ -1,4 +1,12 @@
 package com.NomadNook.NomadNook.Security.DTO.RESPONSE;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservaResponse {
 }
