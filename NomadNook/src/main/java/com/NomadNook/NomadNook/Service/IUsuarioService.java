e0956cc43dcf38b1aa8eeea98d62f1581b0047ed
Juan Oh <jioh1982@gmail.com>
2025-02-27 17:08:23 -0300

    Comentarios de SecurityConfig

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IUsuarioService.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IUsuarioService.java
@@ -2,12 +2,13 @@ package com.NomadNook.NomadNook.Service;
 
 import com.NomadNook.NomadNook.DTO.REQUEST.UsuarioRequest;
 import com.NomadNook.NomadNook.DTO.RESPONSE.UsuarioResponse;
+import com.NomadNook.NomadNook.Model.Usuario;
 
 import java.util.List;
 
 public interface IUsuarioService {
 
-    UsuarioResponse createUser(UsuarioRequest usuarioRequest);
+    void createUser(List<Usuario> usuarios);
     UsuarioResponse getUserById(Long id);
     List<UsuarioResponse> listAllUsers();
     UsuarioResponse updateUser(Long id, UsuarioRequest usuarioRequest);
