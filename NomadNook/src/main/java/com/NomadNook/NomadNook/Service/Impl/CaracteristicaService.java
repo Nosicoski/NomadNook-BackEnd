8fc11a09aa367aab5da575b7053c486d1766c169
Juan Oh <jioh1982@gmail.com>
2025-02-27 13:05:06 -0300

    Implementacion updateCaracteristica, getCaracteristicaPorId

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Service/Impl/CaracteristicaService.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Service/Impl/CaracteristicaService.java
@@ -2,10 +2,13 @@ package com.NomadNook.NomadNook.Service.Impl;
 
 import com.NomadNook.NomadNook.DTO.REQUEST.CaracteristicaRequest;
 import com.NomadNook.NomadNook.DTO.RESPONSE.CaracteristicaResponse;
+import com.NomadNook.NomadNook.Exception.ResourceNotFoundException;
 import com.NomadNook.NomadNook.Model.Caracteristica;
 import com.NomadNook.NomadNook.Repository.ICaracteristicaRepository;
 import com.NomadNook.NomadNook.Service.ICaracteristicaService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ import java.util.List;
 public class CaracteristicaService implements ICaracteristicaService {
 
     private final ICaracteristicaRepository caractereisticaRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(CaracteristicaService.class);
     private final ModelMapper modelMapper;
 
     public CaracteristicaService(ICaracteristicaRepository caractereisticaRepository, ModelMapper modelMapper) {
@@ -40,7 +44,10 @@ public class CaracteristicaService implements ICaracteristicaService {
 
     @Override
     public CaracteristicaResponse getCaracteristicaById(Long id) {
-        return null;
+        Caracteristica caracteristica = caractereisticaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontró la característica con id: " + id));
+
+        return createCaracteristicaResponse(caracteristica);
     }
 
     @Override
@@ -56,7 +63,19 @@ public class CaracteristicaService implements ICaracteristicaService {
 
     @Override
     public CaracteristicaResponse updateCaracteristica(Long id, CaracteristicaRequest request) {
-        return null;
+        Caracteristica existingCaracteristica = caractereisticaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontró la característica con id: " + id));
+
+        if(!existingCaracteristica.getNombre().equals(request.getNombre())
+                && caractereisticaRepository.existsByNombre(request.getNombre())) {
+            throw new IllegalArgumentException("Ya existe una caracteristica con el mismo nombre");
+        }
+
+        modelMapper.map(request, existingCaracteristica);
+
+        Caracteristica updatedCaracteristica = caractereisticaRepository.save(existingCaracteristica);
+        LOGGER.info("Caracteristica actualizada con id: {}", updatedCaracteristica.getId());
+        return createCaracteristicaResponse(updatedCaracteristica);
     }
 
     @Override
