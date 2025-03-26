491e668e98c0bd51f67e0ee973da4b394b8f537e
Juan Oh <jioh1982@gmail.com>
2025-03-26 18:27:19 -0300

    Feature(Reservas): Se envia un mail al cliente cada vez que se crea una reserva

--- NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IEmailService.java
+++ NomadNook/src/main/java/com/NomadNook/NomadNook/Service/IEmailService.java
@@ -1,8 +1,9 @@
 package com.NomadNook.NomadNook.Service;
 
+import com.NomadNook.NomadNook.DTO.RESPONSE.ReservaResponse;
 import jakarta.mail.MessagingException;
 
 public interface IEmailService {
     void sendConfirmationEmail(String to, String username) throws MessagingException;
-
+    void sendReservaEmail(ReservaResponse reserva) throws MessagingException;
 }
